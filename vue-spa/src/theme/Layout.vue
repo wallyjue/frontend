1330332c79588e8c980284e159e0560b310a3cd0
wallyjue <wallyjue@gmail.com>
2018-01-16 21:44:13 +0900

    Adding vue spa

---
+++ vue-spa/src/theme/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav class="nav has-shadow">
+      <div class="container">
+        <a href="/">
+          <img src="" alt="Vue SPA" />
+        </a>
+      </div>
+    </nav>
+    <section class="main-section section"></section>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          Follow us on 3
+          <a href="https://twitter.com/bstavroulakis" target="_blank">Twitter</a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
