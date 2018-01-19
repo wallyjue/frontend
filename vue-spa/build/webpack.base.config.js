ff46882a7ef746b85ef3980e24c6f9bd8edfaeff
wally.chang <wally.chang@protolabs.co.jp>
2018-01-19 17:46:53 +0900

    Adding content to vue template

--- vue-spa/build/webpack.base.config.js
+++ vue-spa/build/webpack.base.config.js
@@ -14,7 +14,11 @@ const config = {
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
+        options: {
+          css: 'css-loader',
+          'scss': 'css-loader|sass-loader'
+        }
       },
       {
         test: /\.js$/,
