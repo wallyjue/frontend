d82db5f213edc9a0186c953426cea9fad8e5ad5b
wally.chang <wally.chang@protolabs.co.jp>
2018-01-22 14:58:37 +0900

    Adding slots for the post

---
+++ vue-spa/src/theme/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <slot name="title"></slot>
+      <slot name="content"></slot>
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item" :href="link" target="_blank">Read More</a>
+    </footer>
+  </div>
+</template>
+<script>
+  export default{
+    props: ['link']
+  }
+</script>
