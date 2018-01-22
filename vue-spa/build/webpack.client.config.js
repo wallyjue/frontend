56db5bfcfb8e6b097f327f7a2d744ce59fe34702
wally.chang <wally.chang@protolabs.co.jp>
2018-01-22 15:39:45 +0900

    Separate the style tag from the vue components

--- vue-spa/build/webpack.client.config.js
+++ vue-spa/build/webpack.client.config.js
@@ -9,7 +9,6 @@ config.module.rules
   .filter(x => { return x.loader == 'vue-loader'})
   .forEach(x => x.options.extractCSS = true)
 
-
 config.plugins.push(
   new ExtractTextPlugin('assets/styles.css')
 )
