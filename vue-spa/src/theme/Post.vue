7e6369446ef3dd8a9f8ef263c53d6bb418e20705
wally.chang <wally.chang@protolabs.co.jp>
2018-01-22 15:29:36 +0900

    Adding new plugin

--- vue-spa/src/theme/Post.vue
+++ vue-spa/src/theme/Post.vue
@@ -14,3 +14,15 @@
     props: ['link']
   }
 </script>
+<style scoped>
+.card{
+  padding-bottom: 40px;
+  height: 100%;
+}
+footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+}
+</style>
