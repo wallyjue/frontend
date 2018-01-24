d1ce7ca461273fe0648da0ee5454025fdc94f607
wally.chang <wally.chang@protolabs.co.jp>
2018-01-24 14:17:47 +0900

    formatting

--- vue-spa/src/app.service.js
+++ vue-spa/src/app.service.js
@@ -11,6 +11,14 @@ const appService = {
         })
     })
   },
+  getProfile () {
+    return new Promise((resolve) => {
+      axios.get('/services/profile.php')
+        .then(response => {
+          resolve(response.data)
+        })
+    })
+  },
   login (credentials) {
     return new Promise((resolve, reject) => {
       axios.post('/services/auth.php', credentials).then(response => {
